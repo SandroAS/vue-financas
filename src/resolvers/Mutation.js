be4385cb6261ac64eb8672a175e6e80fcd6e4645
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-04-27 14:12:35 -0300

    createAccount mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,22 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { getUserId } = require('./../utils')
 
 const JWT_SECRET = process.env.JWT_SECRET
+
+async function createAccount (_, { description }, ctx, info) {
+  const userId = getUserId(ctx)
+  return ctx.db.mutation.createAccount({
+    data: {
+      description,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  }, info)
+}
 
 async function login (_, { email, password }, ctx, info) {
 
@@ -40,5 +55,6 @@ async function signup (_, args, ctx, info) {
 
 module.exports = {
   signup,
-  login
+  login,
+  createAccount
 }
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,8 @@
-function user(_, args, ctx, info) {
-  return ctx.db.query.user({ where: { id: args.id }}, info)
+const { getUserId } = require('./../utils')
+
+function user (_, args, ctx, info) {
+  const userId = getUserId(ctx)
+  return ctx.db.query.user({ where: { id: userId }}, info)
 }
 
 module.exports = {
